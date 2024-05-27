eb46bd0385abcac97cadc48b7bd86a44bd6daebc
TechThiha <thiha95.tech@gmail.com>
2024-05-27 22:45:45 +0700

    error fix

--- routes/console.php
+++ routes/console.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspires', function () {
+Artisan::command('inspiress', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
